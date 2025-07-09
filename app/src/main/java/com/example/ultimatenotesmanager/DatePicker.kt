dd0443489390052d7d7bda564b8398ece2b740ed
Moony002 <andrey.sablin.2004@mail.ru>
2025-07-09 17:43:46 +0300

    Added date picker screen.

--- app/src/main/java/com/example/ultimatenotesmanager/DatePicker.kt
+++ app/src/main/java/com/example/ultimatenotesmanager/DatePicker.kt
@@ -1,12 +1,12 @@
-//package com.example.ultimatenotesmanager
-//
-//import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//
-//class DatePicker : AppCompatActivity() {
+package com.example.ultimatenotesmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DatePicker : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
@@ -17,4 +17,4 @@
 //            insets
 //        }
 //    }
-//}
+}
